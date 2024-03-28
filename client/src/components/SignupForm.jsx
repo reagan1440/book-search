9a51b621d07fda5fce585846f08a7343e8ba275d
reagan1440 <122570333+reagan1440@users.noreply.github.com>
2024-03-28 19:27:28 -0400

    fixed import issue

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
 import { ADD_USER } from '../utils/mutations';
 import { useMutation } from "@apollo/client";
 
